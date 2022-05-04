26309bb2860f0e8e2496105ac1eb27d3d723e752
Mustafa <mustafa_sahinci@hotmail.com>
2022-05-04 13:43:34 +0000

    creating api

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const API_KEY = "31Q8BjbuMhc5Rg2_m2j26YA0VUc";
+const API_URL = "https://www.ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"))
